de1b8bd1fd80aeeb14739f6ce5137bfaede1b755
Adipah Osei <adipahosei@gmail.com>
2024-05-31 20:10:23 +0000

    added profile and search bar section

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,15 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title='Tap' />
-      <View>
-        
+      <View style = {styles.profileContainer}>
+        <View>
+          <Text style = {{fontSize: 30, fontWeight: 'bold', marginBottom: 2}}>Hello,   Devs </Text>
+          <Text>14 task for today</Text>
+        </View>
+        <View>
+          <Image source={require('./assets/person.png')} style = {{marginRight: 5 }} />
+        </View>
       </View>
+      <View>
+        <Image source={require('./assets/Search.png')} style = {{marginBottom: 50}}/>
+      </View>
+      <Text style = {{fontSize: 30, fontWeight: 'bold', marginBottom: 2}}>Categories</Text>
     </View>
   );
 }
@@ -17,8 +23,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8D1BA',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f0e2d3',
+    paddingTop: 70,
+    paddingHorizontal: 25
   },
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 70,
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  
 });
